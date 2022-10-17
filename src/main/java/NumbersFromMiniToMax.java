c36f0a7d78df0f65069590a4107b4a6153d1957c
Nlukasevica <Lukasevica78@gmail.com>
2022-10-17 12:15:03 -0500

    Lesson 7 -  commit code

---
+++ src/main/java/NumbersFromMiniToMax.java
@@ -0,0 +1,11 @@
+public class NumbersFromMiniToMax {
+   public static void main (String [] args){
+
+       int[] numbers = {2, 76, 9, 90, 31, 76, 6, 88, 21, 12,-8};
+       for(int i=10;i>=0;i--) {
+
+           System.out.println (numbers[i]);
+       }
+
+   }
+}
